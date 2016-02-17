94e49b33556ef48ccc1c122d2df83d0bdd83695b
QuestZhang <zs_new@126.com>
2016-02-17 20:32:15 +0800

    第一个ios程序
    一些学习的demo

--- HelloWord/nineG/NineController.m
+++ HelloWord/nineG/NineController.m
@@ -9,6 +9,8 @@
 #import "NineController.h"
 
 #define kColumn 3
+#define kTopY 5
+#define imageViewWidth 45
 
 @interface NineController ()
 
@@ -41,6 +43,59 @@
             [self.view addSubview:yellowView];
             
             NSLog(@"9宫格");
+            
+            //内部子控件的添加
+            
+            //添加ImageView
+            
+            //确定宽高
+            CGFloat imageViewX = (yellowWidth - imageViewWidth)/2;
+            UIImageView * iconImageView = [[UIImageView alloc] initWithFrame:CGRectMake(imageViewX, kTopY, imageViewWidth, imageViewWidth)];
+            
+            [iconImageView setBackgroundColor:[UIColor redColor]];
+            
+            [yellowView addSubview:iconImageView];
+            
+            //添加Label
+            
+            //LabelY轴
+            CGFloat labelY = CGRectGetMaxY(iconImageView.frame)+kTopY/2;//想获得最大Y值
+            
+            
+            UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, labelY, yellowWidth, 15)];
+             nameLabel.text = @"爸爸去";
+            nameLabel.textAlignment = NSTextAlignmentCenter;
+            
+            [yellowView addSubview:nameLabel];
+            
+            
+            //设置字体
+            nameLabel.font = [UIFont systemFontOfSize:15]; //经常使用
+            [UIFont boldSystemFontOfSize:15];//粗体
+            [UIFont italicSystemFontOfSize:15];//斜体(对中文不支持)
+            
+            //添加button
+            
+            CGFloat buttonWidth = imageViewWidth + 10;
+            CGFloat buttonHeight = 20;
+            
+            CGFloat buttonX = (yellowWidth - buttonWidth)/2;
+            CGFloat buttonY = CGRectGetMaxY(nameLabel.frame)+kTopY/2;
+            
+            UIButton * downloadButton = [[UIButton alloc] initWithFrame:CGRectMake(buttonX , buttonY, buttonWidth, buttonHeight)];
+            
+            //设置button属性
+            //背景图片
+            
+            //设置background image ，image， title 一定要分状态设置
+            /*
+             默认、高亮、被选中、被禁用
+             */
+            
+            downloadButton setBackgroundColor:UI
+            
+            //文字
+            
         }
     }
 }

098f2c1e0e8640b6a7aad39eda0692424f23ca81
QuestZhang <zs_new@126.com>
2016-03-04 21:26:51 +0800

    更新了music
    更新了music

--- HelloWord/player/Mp3ViewController.h
+++ HelloWord/player/Mp3ViewController.h
@@ -6,7 +6,8 @@
 //  Copyright © 2016年 zhangwenqiang. All rights reserved.
 //
 
-#define mp3Url @"http://www.360yys.cn:8090/test/yese.mp3"
+/*#define mp3Url @"http://www.360yys.cn:8090/test/yese.mp3"*/
+#define mp3Url @"http://www.it1224.com/interfaceapp/music/zgwydwd.mp3"
 
 #import <UIKit/UIKit.h>
 
